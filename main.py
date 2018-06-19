6d1c662ac85f60346ed2e28b975dbeae75e7a1ed
GeekSchool-Alexander <geekschool.alexander@gmail.com>
2018-06-19 12:29:29 +0300

    Classwork 19.06.2018

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame as pg
+
+from player import Player
 from settings import *
 
 
@@ -7,9 +9,43 @@ class Game:
 		pg.init()
 		self.screen = pg.display.set_mode(WINDOW_SIZE)
 		self.clock = pg.time.Clock()
+		self.running = True
+	
+	def events(self):
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
+				self.playing = False
+				self.running = False
+	
+	def update(self):
+		self.all_sprites.update()
+	
+	def draw(self):
+		self.screen.fill(WHITE)
+		self.all_sprites.draw(self.screen)
+		pg.display.flip()
+	
+	def run(self):
+		self.playing = True
+		while self.playing:
+			self.clock.tick(FPS)
+			self.events()
+			self.update()
+			self.draw()
+			
+	def new(self):
+		self.all_sprites = pg.sprite.Group()
+		self.player = Player(WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
+		self.all_sprites.add(self.player)
+		self.run()
 		
+	def main(self):
+		while self.running:
+			self.new()
+	
 	def __del__(self):
 		pg.quit()
 		
 
 g = Game()
+g.main()
